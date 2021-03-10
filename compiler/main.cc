063db6b53e2ab563f429e789c6bbf7c15ccd6a79
Robbie VanDerzee <Robert.Scott.VanDerzee@gmail.com>
2021-03-10 14:15:14 -0500

    Configured compiler core logging (#3)

--- compiler/main.cc
+++ compiler/main.cc
@@ -1,4 +1,12 @@
+#include "compiler/logging/log.hpp"
 
 int main(int, char const *[]) {
-    return 0;
+    P::Logging::Log::initialize({
+        .name = "P",
+        .log_directory = "logging/test.log",
+        .max_log_files = 3,
+        .max_log_file_size = 5 * 1024 * 1024,
+        .threads = 8096,
+        .workers = 8,
+    });
 }
